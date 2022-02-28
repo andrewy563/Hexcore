e2897600ec5ce022279a99e2c7524ba0a8ed910f
Andrew Yoon <andrewy563@gmail.com>
2022-02-28 11:59:45 -0800

    add resistance formulas

--- src/Results/component.tsx
+++ src/Results/component.tsx
@@ -1,17 +1,22 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-import { useSelector } from "../redux/hooks";
+import { useSelector } from "src/redux/hooks";
+
+import useTotalDamage from "./hooks/useTotalDamage";
 
 const ResultsColumn = () => {
   const damage = useSelector((state) => state.damage);
   const defense = useSelector((state) => state.defense);
+  const [totalPhysical, totalMagical] = useTotalDamage();
   return (
     <Box sx={{ width: "33%" }}>
       <Typography>Physical Damage: {damage.physicalDamage}</Typography>
       <Typography>Magical Damage: {damage.magicDamage}</Typography>
       <Typography>Physical Armor: {defense.armor}</Typography>
       <Typography>Magic Resistance: {defense.magicResistance}</Typography>
+      <Typography>Total Physical: {totalPhysical}</Typography>
+      <Typography>Total Magical: {totalMagical}</Typography>
     </Box>
   );
 };
---
+++ src/Results/hooks/useTotalDamage.ts
@@ -0,0 +1,22 @@
+import { useSelector } from "src/redux/hooks";
+
+const useTotalDamage = () => {
+  const damage = useSelector((state) => state.damage);
+  const defense = useSelector((state) => state.defense);
+
+  const calculateTotalDamage = (damage, resistance) =>
+    damage / (1 + resistance / 100);
+
+  const totalPhysical = calculateTotalDamage(
+    damage.physicalDamage,
+    defense.armor
+  );
+  const totalMagical = calculateTotalDamage(
+    damage.magicDamage,
+    defense.magicResistance
+  );
+
+  return [totalPhysical, totalMagical];
+};
+
+export default useTotalDamage;
